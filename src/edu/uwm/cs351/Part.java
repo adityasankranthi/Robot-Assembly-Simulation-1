fbbcbe51b3ded796594cff78f1fbca7cc0b950e4
Aditya Sankranthi <127147214+adityasankranthi@users.noreply.github.com>
2024-01-25 03:43:37 -0600

    first draft of changes in TODOs in part.java

--- src/edu/uwm/cs351/Part.java
+++ src/edu/uwm/cs351/Part.java
@@ -9,7 +9,7 @@ import java.util.Random;
  */
 public class Part {
 	// TODO: Declare the (very simple) data structure.
-	
+	private final String serialNumber;
 	
 	/**
 	 * Create a part with the given serial number
@@ -17,17 +17,22 @@ public class Part {
 	 */
 	public Part(String serial) {
 		// TODO: initialize the part after checking for errors.
+		if (serial == null) throw new NullPointerException("Null serial number");
+		this.serialNumber = serial;
 	}
 	
 	/**
 	 * Create a part with a random serial number.
 	 */
 	public Part() {
+		this("" + new Random().nextInt());
 		// TODO: call the other constructor with "this(...)"
 		// filling in the "..." with a random string which
 		// can indeed be a series of digits.
 		// Our solution uses "new Random().nextInt()" as part of this.
+		
 	}
+	
 	
 	// TODO: Methods of the class
 }
