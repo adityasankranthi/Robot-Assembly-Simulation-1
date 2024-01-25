8704e0ae8b8cf8f3ede482c73981df8356e85237
Aditya Sankranthi <127147214+adityasankranthi@users.noreply.github.com>
2024-01-25 15:30:06 -0600

    Stub to unlock tests

--- src/edu/uwm/cs351/ArrayRobot.java
+++ src/edu/uwm/cs351/ArrayRobot.java
@@ -11,4 +11,35 @@ public class ArrayRobot implements Robot {
 	private Part[] parts;
 	
 	// TODO: First stub and then write this class
+	//required
+	public ArrayRobot() {
+		
+	}
+	
+	// required
+	public ArrayRobot(int x) {
+		
+	}
+	
+	@Override // required
+	public boolean addPart(String function, Part part) {
+		// TODO
+		return false;
+	}
+	
+	@Override // required
+	public Part removePart(String function) {
+		Part result = null;
+		// TODO
+		return result;
+	}
+	
+	@Override // required
+	public Part getPart(String function, int index) {
+		Part result = null;
+		// TODO
+		return result;
+	}
+
+
 }
