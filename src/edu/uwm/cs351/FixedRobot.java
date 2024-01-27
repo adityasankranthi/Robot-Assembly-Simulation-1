37c672e589dc16d847708bbd4f77b7971fe19d93
Aditya Sankranthi <127147214+adityasankranthi@users.noreply.github.com>
2024-01-26 23:21:03 -0600

    First draft of removePart()

--- src/edu/uwm/cs351/FixedRobot.java
+++ src/edu/uwm/cs351/FixedRobot.java
@@ -19,45 +19,83 @@ public class FixedRobot implements Robot {
 		// nothing to do
 	}
 	
-	@Override // required
-	public boolean addPart(String function, Part part) {
-		// TODO
-		if (function == null || part == null) throw new NullPointerException("The parameters can't be empty");
-		
-		if (function.equals(ARM)) {
-	        if (arm1 == null) {
-	            arm1 = part;
-	            return true;
-	        } else if (arm2 == null) {
-	            arm2 = part;
-	            return true;
-	        }
-	    } else if (function.equals(LEG)) {
-	        if (leg1 == null) {
-	            leg1 = part;
-	            return true;
-	        } else if (leg2 == null) {
-	            leg2 = part;
-	            return true;
-	        }
-	    } else if (otherFunction == null) {
+	@Override
+    public boolean addPart(String function, Part part) {
+        if (function == null || part == null) throw new NullPointerException("Function and part cannot be null");
+        if (function.equals(ARM)) {
+            if (arm1 == null) {
+                arm1 = part;
+                return true;
+            } else if (arm2 == null) {
+                arm2 = part;
+                return true;
+            }
+        } else if (function.equals(LEG)) {
+            if (leg1 == null) {
+                leg1 = part;
+                return true;
+            } else if (leg2 == null) {
+                leg2 = part;
+                return true;
+            }
+        } else {
 	        otherFunction = function;
 	        otherPart = part;
 	        return true;
-	    }
+        }
+        return false;
+    }
 
-	    return false; 
-	}
-	
-	@Override // required
-	public Part removePart(String function) {
-		Part result = null;
-		
-		
-		// TODO
-		return result;
-	}
-	
+    @Override
+    public Part removePart(String function) {
+        Part removedPart = null;
+        if (function.equals(null)) {
+            if (arm1 != null) {
+                removedPart = arm1;
+                arm1 = null;
+            } 
+            else if (arm2 != null) {
+                removedPart = arm2;
+                arm2 = null;
+            }
+            else if (leg1 != null) {
+                removedPart = leg1;
+                leg1 = null;
+            } 
+            else if (leg2 != null) {
+                removedPart = leg2;
+                leg2 = null;
+            }
+            else if(otherPart != null) {
+                removedPart = otherPart;
+                otherPart = null;
+            }
+        }
+        else if (function.equals(ARM)) {
+            if (arm1 != null) {
+                removedPart = arm1;
+                arm1 = null;
+            } else if (arm2 != null) {
+                removedPart = arm2;
+                arm2 = null;
+            }
+        } else if (function.equals(LEG)) {
+            if (leg1 != null) {
+                removedPart = leg1;
+                leg1 = null;
+            } else if (leg2 != null) {
+                removedPart = leg2;
+                leg2 = null;
+            }
+        } else if (function.equals(otherFunction)){
+            if (otherPart != null) {
+                removedPart = otherPart;
+                otherPart = null;
+            }
+        }
+        return removedPart;
+    }
+
 	@Override // required
 	public Part getPart(String function, int index) {
 		Part result = null;
