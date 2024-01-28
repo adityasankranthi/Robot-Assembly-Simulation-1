0c91b85ddbd96bb4c8263813aab3cb47a084bc66
Aditya Sankranthi <127147214+adityasankranthi@users.noreply.github.com>
2024-01-27 23:00:27 -0600

    implementation of ArrayRobot

--- src/edu/uwm/cs351/ArrayRobot.java
+++ src/edu/uwm/cs351/ArrayRobot.java
@@ -5,6 +5,7 @@ package edu.uwm.cs351;
  * that can maintain a fixed number of parts
  * of any type.
  */
+
 public class ArrayRobot implements Robot {
 	public static final int DEFAULT_CAPACITY = 6;
 	private String[] functions;
@@ -13,33 +14,78 @@ public class ArrayRobot implements Robot {
 	// TODO: First stub and then write this class
 	//required
 	public ArrayRobot() {
-		
+		this(DEFAULT_CAPACITY);
 	}
 	
 	// required
-	public ArrayRobot(int x) {
-		
+	public ArrayRobot(int capacity) {
+		functions = new String[capacity];
+		parts = new Part[capacity];
 	}
 	
 	@Override // required
 	public boolean addPart(String function, Part part) {
-		// TODO
-		return false;
-	}
+        if (function == null || part == null) throw new NullPointerException("Function and part cannot be null");
+        for (int i = 0; i < parts.length; i++) {
+            if (parts[i] == null) {
+                functions[i] = function;
+                parts[i] = part;
+                return true;
+            }
+        }
+        return false;
+    }
 	
 	@Override // required
 	public Part removePart(String function) {
-		Part result = null;
-		// TODO
-		return result;
+		if (function == null) {
+			for (int i = 0; i < functions.length; i++) {
+	            if (functions[i] != null) {
+	                Part removedPart = parts[i];
+	                functions[i] = null;
+	                parts[i] = null;
+	                return removedPart;
+	            }
+			}
+		}
+		else {
+			for (int i = 0; i < functions.length; i++) {
+	            if (functions[i] != null && functions[i].equals(function)) {
+	                Part removedPart = parts[i];
+	                functions[i] = null;
+	                parts[i] = null;
+	                return removedPart;
+	            }
+			}
+        }
+        return null; 
 	}
 	
 	@Override // required
 	public Part getPart(String function, int index) {
+		if (index < 0 ) throw new IllegalArgumentException("index can't be negative");
 		Part result = null;
-		// TODO
-		return result;
-	}
-
-
+		int count = 0;
+		if (function == null) {
+			for (int i = 0; i < functions.length; i++) {
+	            if (functions[i] != null) {
+	                if (count == index) {
+	                    return parts[i];
+	                }
+	                count++;
+	            }
+	        }
+		}
+		else {
+			for (int i = 0; i < functions.length; i++) {
+	            if (functions[i] != null && functions[i].equals(function)) {
+	                if (count == index) {
+	                    return parts[i];
+	                }
+	                count++;
+	            }
+	        }
+		}
+        return result;
+    }
 }
